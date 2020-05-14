c6eea3b74e3ec602bfe977808d9570abf6d06b65
RaphaelSouzaPereira <raphaelsouzapereira32@gmail.com>
2020-05-13 21:40:43 -0300

    alterando nome da pasta

---
+++ app/src/pages/Mecanica.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View } from 'react-native';
+import { WebView  } from 'react-native-webview';
+
+function Mecanica({navigation}){
+   //const nomeDoParametro = navigation.getParam('nomedoparametro');
+
+   return <View />
+}
+
+export default Mecanica;
