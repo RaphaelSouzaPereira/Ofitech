84cdce11b09536f1382d3bcc55e9a3c5a971e095
RaphaelSouzaPereira <raphaelsouzapereira32@gmail.com>
2020-05-17 14:30:49 -0300

    retirando pastas duplicadas

--- avaliacao/src/routes.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require("express");
-const AvaliacaoController = require("./controllers/AvaliacaoController");
-const routes = Router();
-
-routes.get("/api/avaliacao", AvaliacaoController.index);
-routes.post("/api/avaliacao", AvaliacaoController.store);
-
-module.exports = routes;
--- mecanica/src/routes.js
+++
@@ -1,7 +0,0 @@
-const { Router } = require("express");
-const MecanicaController = require("./controllers/MecanicaController");
-const routes = Router();
-
-routes.post("/api/mecanica", MecanicaController.store);
-
-module.exports = routes;
