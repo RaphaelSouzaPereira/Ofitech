da50e0c5be5bbaa44615db52671a709fcbd94043
RaphaelSouzaPereira <raphaelsouzapereira32@gmail.com>
2020-05-17 18:05:58 -0300

    arrumando bug na api

--- api_geolocalizacao/src/controllers/SearchController.js
+++ api_geolocalizacao/src/controllers/SearchController.js
@@ -1,46 +1,44 @@
-const Mecanica = require('../models/Mecanica');
-const parseStringAsArray = require('../utils/parseStringAsArray');
+const Mecanica = require("../models/Mecanica");
+const parseStringAsArray = require("../utils/parseStringAsArray");
 
 module.exports = {
-    async get(request, response) {
-        const { latitude, longitude, servicos } = request.query;
-        let mecanicas;
+  async get(request, response) {
+    const { latitude, longitude, servicos } = request.query;
+    let mecanicas;
 
-        if (servicos !== 'undefined' && servicos !== null && servicos.length > 0) {
-            const servicosArray = parseStringAsArray(servicos);
+    if (typeof servicos !== "undefined" && servicos !== null) {
+      const servicosArray = parseStringAsArray(servicos);
 
-             mecanicas = await Mecanica.find({
-                servicos: {
-                    $in: servicosArray, //busca por serviços
-                },
-                location: {
-                    $near: {
-                        $geometry: { //busca a localização
-                            type: 'Point',
-                            coordinates: [longitude, latitude],
-                        },
-                        $maxDistance: 10000,
-                    },
-                },
-
-            });
-
-        } else {
-             mecanicas = await Mecanica.find({
-                location: {
-                    $near: {
-                        $geometry: { //busca a localização
-                            type: 'Point',
-                            coordinates: [longitude, latitude],
-                        },
-                        $maxDistance: 10000,
-                    },
-                },
-
-            });
-        }
-
-        return response.json({ mecanicas });
-
+      mecanicas = await Mecanica.find({
+        servicos: {
+          $in: servicosArray, //busca por serviços
+        },
+        location: {
+          $near: {
+            $geometry: {
+              //busca a localização
+              type: "Point",
+              coordinates: [longitude, latitude],
+            },
+            $maxDistance: 10000,
+          },
+        },
+      });
+    } else {
+      mecanicas = await Mecanica.find({
+        location: {
+          $near: {
+            $geometry: {
+              //busca a localização
+              type: "Point",
+              coordinates: [longitude, latitude],
+            },
+            $maxDistance: 10000,
+          },
+        },
+      });
     }
-}
+
+    return response.json({ mecanicas });
+  },
+};
